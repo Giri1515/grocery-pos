94f80430b2b4eb5220e55261e4b5c3286873ff58
Barry Tarlton <btarlton@gmail.com>
2018-07-30 08:56:27 -0400

    initial commit of starter project created from start.spring.io

---
+++ src/main/java/com/barry/grocerypos/GroceryPosApplication.java
@@ -0,0 +1,12 @@
+package com.barry.grocerypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryPosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/barry/grocerypos/GroceryPosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.barry.grocerypos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroceryPosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
