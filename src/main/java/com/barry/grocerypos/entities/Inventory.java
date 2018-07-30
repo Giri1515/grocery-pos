68aaff287bd73bc29f8efde8e442df34c9283ecb
Barry Tarlton <btarlton@gmail.com>
2018-07-30 15:52:01 -0400

    addItem added to Inventory

--- src/main/java/com/barry/grocerypos/entities/Inventory.java
+++ src/main/java/com/barry/grocerypos/entities/Inventory.java
@@ -1,12 +1,22 @@
 package com.barry.grocerypos.entities;
 
+import java.math.BigDecimal;
+
 public class Inventory {
+	
 	
 	
 	public Item getItemByName(String name) {
 		Item item = new Item();
 		item.setName(name);
+		item.setPrice(new BigDecimal(10.00));
 		return item;
+	}
+	
+	public void addItem(String name, double price) {
+		
+		
+		
 	}
 
 }
--- src/test/java/com/barry/grocerypos/entities/InventoryTest.java
+++ src/test/java/com/barry/grocerypos/entities/InventoryTest.java
@@ -2,6 +2,8 @@ package com.barry.grocerypos.entities;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
 public class InventoryTest {
@@ -17,6 +19,22 @@ public class InventoryTest {
 		
 		
 	}
+	
+
+	@Test
+	public void itemCanBeAddedToInventoryWithNameAndPrice() {
+		Inventory inventory = new Inventory();
+		
+		inventory.addItem("Bacon", 10.00);
+		
+		Item retrievedItem = inventory.getItemByName("Bacon");
+		
+		assertEquals("Bacon", retrievedItem.getName());
+		assertEquals(new BigDecimal(10.00), retrievedItem.getPrice());
+		
+		
+	}
+
 	
 	
 
