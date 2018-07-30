2dcebd2d23e7be76a442cc484f5d96d3196f8137
Barry Tarlton <btarlton@gmail.com>
2018-07-30 16:00:18 -0400

    Inventory updated to hold map of items

--- src/main/java/com/barry/grocerypos/entities/Inventory.java
+++ src/main/java/com/barry/grocerypos/entities/Inventory.java
@@ -1,20 +1,27 @@
 package com.barry.grocerypos.entities;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Inventory {
 	
 	
+	private Map<String, Item> itemMap = new HashMap<>();
+
+	
 	
 	public Item getItemByName(String name) {
-		Item item = new Item();
-		item.setName(name);
-		item.setPrice(new BigDecimal(10.00));
-		return item;
+		return itemMap.get(name);
 	}
 	
 	public void addItem(String name, double price) {
 		
+		Item newItem = new Item();
+		newItem.setName(name);
+		newItem.setPrice(new BigDecimal(price));
+		
+		itemMap.put(name, newItem);
 		
 		
 	}
--- src/test/java/com/barry/grocerypos/entities/InventoryTest.java
+++ src/test/java/com/barry/grocerypos/entities/InventoryTest.java
@@ -13,6 +13,8 @@ public class InventoryTest {
 	public void inventoryCanRetrieveItemByName() {
 		Inventory inventory = new Inventory();
 		
+		inventory.addItem("Bacon", 0.00);
+		
 		Item retrievedItem = inventory.getItemByName("Bacon");
 		
 		assertEquals("Bacon", retrievedItem.getName());
@@ -32,8 +34,26 @@ public class InventoryTest {
 		assertEquals("Bacon", retrievedItem.getName());
 		assertEquals(new BigDecimal(10.00), retrievedItem.getPrice());
 		
+	}
+	
+	@Test
+	public void whenAdding2ItemsToInventoryCanRetrieveBothByName() {
+		Inventory inventory = new Inventory();
+		
+		inventory.addItem("Bacon", 10.00);
+		inventory.addItem("Cheese", 5.00);
+		
+		Item baconItem = inventory.getItemByName("Bacon");
+		Item cheeseItem = inventory.getItemByName("Cheese");
+		
+		assertEquals("Bacon", baconItem.getName());
+		assertEquals(new BigDecimal(10.00), baconItem.getPrice());
+		assertEquals("Cheese", cheeseItem.getName());
+		assertEquals(new BigDecimal(5.00), cheeseItem.getPrice());
+		
 		
 	}
+
 
 	
 	
