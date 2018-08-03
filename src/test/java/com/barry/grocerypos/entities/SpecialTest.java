a97e9f438231457ddd1d4aee7ed20d8166e87447
Barry Tarlton <btarlton@gmail.com>
2018-08-03 08:33:08 -0400

    removed some unused imports from SpecialTest

--- src/test/java/com/barry/grocerypos/entities/SpecialTest.java
+++ src/test/java/com/barry/grocerypos/entities/SpecialTest.java
@@ -1,13 +1,10 @@
 package com.barry.grocerypos.entities;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
